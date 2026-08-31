4d11024d825614127292e61fd8cd3da873175d8c
matheusfelipe1298 <matheusfelipeambrosiodossantos@gmail.com>
2026-08-31 10:50:02 -0300

    Organiza select-where

--- 01 - select-where/Select-where.sql
+++ 01 - select-where/Select-where.sql
@@ -1,20 +1,4 @@
-CREATE DATABASE estudos_sql;
-USE estudos_sql;
-
-CREATE TABLE produtos (
-    id INT PRIMARY KEY,
-    nome VARCHAR(100),
-    categoria VARCHAR(50),
-    preco DECIMAL(10,2),
-    quantidade INT
-);
-
-INSERT INTO produtos VALUES
-(1, 'Mouse', 'Eletronicos', 100.00, 10),
-(2, 'Teclado', 'Eletronicos', 150.00, 5),
-(3, 'Cadeira', 'Moveis', 800.00, 3),
-(4, 'Fone', 'Eletronicos', 70.00, 15),
-(5, 'Mesa', 'Moveis', 500.00, 4);
+USE estudos_sql; 
 
 SELECT *
 FROM produtos;
